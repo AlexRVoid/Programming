31d3740a3159380dcd113fe11bea09b12e6b65bc
Alexandr <vtyurinalexandr@yahoo.com>
2022-05-26 11:18:54 +0700

    Create Algorithms(m).h

---
+++ Lab7/Algorithms(m).h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+template <typename T, typename T1>
+
+ostream& operator<< (ostream& os, const map<T, T1>& v) {
+    for (auto i : v) {
+        os << i.first << " " << i.second << endl;
+    }
+    return os;
+}
+
+
+void Algm();
