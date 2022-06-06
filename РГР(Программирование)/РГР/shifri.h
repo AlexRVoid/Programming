ca2b919a1f1716b86ef509670b21c945033b5755
Alexandr <vtyurinalexandr@yahoo.com>
2022-06-06 10:11:23 +0700

    Create shifri.h

---
+++ РГР(Программирование)/РГР/shifri.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <ctime>
+#include <string>
+#include <iostream>
+#include <conio.h>
+#include <vector>
+#include <ranges>
+#include <Windows.h>
+#include <cmath>
+#include <fstream>
+using namespace std;
+void gronsf(string s);
+void tarab();
+void vern(string s);
+int trit(string s);
+void vizh(string s);
+void rsa(string s);
