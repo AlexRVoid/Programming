c5f8c529984d3ec6561220441e11c1edd6497cff
Alexandr <vtyurinalexandr@yahoo.com>
2022-06-07 18:50:11 +0700

    Create Lad5.cs

---
+++ Lab5/Lab5 второй язык/Lad5.cs
@@ -0,0 +1,2 @@
+﻿//C++ TO C# CONVERTER WARNING: The original C++ template specifier was replaced with a C# generic specifier, which may not produce the same behavior:
+//ORIGINAL LINE: template <typename T, typename T1>
