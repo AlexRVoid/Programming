366e5c0e7c85b91cda8b2333cae168b5b26cd8dc
Alexandr <vtyurinalexandr@yahoo.com>
2022-06-07 08:00:34 +0700

    Create RandomNumbers.cs

---
+++ Лаба2/Lab2 Си шарп/RandomNumbers.cs
@@ -0,0 +1,31 @@
+﻿
+internal static class RandomNumbers
+{
+	private static System.Random r;
+
+	public static int NextNumber()
+	{
+		if (r == null)
+			Seed();
+
+		return r.Next();
+	}
+
+	public static int NextNumber(int ceiling)
+	{
+		if (r == null)
+			Seed();
+
+		return r.Next(ceiling);
+	}
+
+	public static void Seed()
+	{
+		r = new System.Random();
+	}
+
+	public static void Seed(int seed)
+	{
+		r = new System.Random(seed);
+	}
+}
