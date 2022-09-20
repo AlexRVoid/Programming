66cf0bbd2ae0c415843402cc828767b994345859
Alexandr <vtyurinalexandr@yahoo.com>
2022-09-20 11:06:43 +0700

    прототип проекта (подключение к бд "login page + Survey page")
    Требуется доработка подключения к базе данных (Ошибка при подключении)

--- Bootstrap project/index.php
+++ Bootstrap project/index.php
@@ -1,40 +1,57 @@
+<?php
+session_start();
+$nameServer = 'localhost';
+$userName = 'Alex';
+$password = '1111';
+$DBname = '';
+$mysqli = new mysqli($nameServer, $userName, $password, $DBname);
+?>
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="twitter:card" content="summary_large_image"/>
-    <meta name="twitter:title" content="Страница входа">
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
-    <link rel="shortcut icon" href="assets/images/logo.png" type="image/x-icon">
-    <meta name="description" content="login page">
-    <title>login page</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
-    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,400;0,700;1,400;1,700&display=swap&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
-    <noscript><link rel="stylesheet" href=" https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,400;0,700;1,400;1,700&display=swap&display=swap">
-    </noscript>
-    </head>
-    <body>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="twitter:card" content="summary_large_image"/>
+<meta name="twitter:title" content="Страница входа">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+<link rel="shortcut icon" href="assets/images/logo.png" type="image/x-icon">
+<meta name="description" content="login page">
+<title>Login page</title>
+<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+<link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+<link rel="preload" href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,400;0,700;1,400;1,700&display=swap&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<noscript><link rel="stylesheet" href=" https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,400;0,700;1,400;1,700&display=swap&display=swap">
+</noscript>
+</head>
+<body>
 		
-		<div class="container">
+	<div class="container">
+		
 		
 		<nav class="navbar navbar-light bg-light">
-		<a class="navbar-brand" href="#">
+		<a class="navbar-brand" href="https://www.romona.ru">
 			<img src="assets/img/logo.png" class="d-inline-block align-top">
 		</a>
 		</nav>
 		
-        <form>
-            <div class="form-group">
-              <label for="Login-Input">Имя</label>
-              <input type="text" class="form-control" id="Login-Input" placeholder="Имя">
-            </div>
-            <div class="form-group">
-              <label for="Password-Input">Пароль</label>
-              <input type="password" class="form-control" id="Password-Input" placeholder="Пароль">
-            </div>
-            <button type="submit" class="btn btn-primary" href = "Survey.php">Submit</button>
-          </form>
-		  
-		  </div>
-    </body>
+       
+		<form action = "database.php" method="post">
+           
+			<div class="form-group">
+			<label for="User">Имя</label>
+			<input type="text" class="form-control" id="User" placeholder="Имя">
+			</div>
+			
+			<div class="form-group">
+			<label for="Password">Пароль</label>
+			<input type="password" class="form-control" id="Password" placeholder="Пароль">
+			</div>
+		
+			<button type="submit" class="btn btn-primary">Submit</button>
+			
+		</form>
+		
+	
+	</div>
+</body>
+	
