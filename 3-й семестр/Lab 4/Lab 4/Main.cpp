0c55c2d3f32c1dc88314dccbcb25258c1552aa9e
Alexandr <vtyurinalexandr@yahoo.com>
2022-12-02 12:47:03 +0700

    lab4

--- 3-й семестр/Lab 4/Lab 4/Main.cpp
+++ 3-й семестр/Lab 4/Lab 4/Main.cpp
@@ -4,52 +4,158 @@
 #include <fstream>
 #include <windows.h>
 #include <map>
+#include <ctime>
 
 using namespace std;
 
 class Applications
 {
-public: 
-    map <string, int> members = { {"Papa ", 1},
-                            {"Mama ", 2},
-                            {"Kate ", 3} };
-    friend ostream& operator<<(const Applications& applications, ostream& os)
+public:
+    friend ostream& operator<< (ostream& os, const Applications& applications)
     {
-        os << "Member\t Member number\n";
-        map<string, int>::const_iterator it;
+        os << "Member number\tMember\n";
+        map<int, string>::const_iterator it;
         for (it = applications.members.begin(); it != applications.members.end(); ++it)
-            os << (*it).first << " " << (*it).second << "\n";
+            os << (*it).first << "\t " << (*it).second << "\n";
         return os;
     }
-    
-    
-    friend void AddMember(Applications& applications, pair <string, int>& member);
-    void GetMembers();
-    
-private:
-    
-    pair <string, int> member = {"aaa",123};
+
+    /*friend istream& operator>>(istream& is, Applications& applications)
+    {
+        is << 
+        return is;
+    }*/
+
     map <int, int> result;
+
+    int GenMemberNumber();
+    friend void AddMember(Applications& applications, pair <int, string>& member);
+    void GetMembers();
+    friend void AddResult(Applications& applications, pair <int, int>& member);
+    void FindMember(int membernumber);
+
+private:
+    map <int, string> members;
+    
+};
+class LogIn
+{
+
 };
 
-void AddMember(Applications& applications, pair <string, int>& member)
+
+void AddMember(Applications& applications, pair <int, string>& member)
 {
     applications.members.insert(member);
+    applications.result.insert(member.first, NULL);
 }
 
 void Applications::GetMembers()
 {
+    cout << "Member number\t Member" << endl;
+    for (auto i : members)
+    {
+        cout << i.first << "\t " << i.second << endl;
+    }
+}
+
+void AddResult(Applications& applications, pair <int, int>& member)
+{
+    if (applications.result.find(member.first) != applications.result.end())
+    {
+        map <int, int> ::iterator it;
+        it = applications.result.find(member.first);
+        (*it).second = member.second;
+    }
+    else cout << "There is no such member";
+    system("pause");
+}
+
+int Applications::GenMemberNumber()
+{
+    while (true)
+    {
+        srand(time(NULL));
+        int membernum = rand() % 1000;
+        map <int, string> ::iterator it;
+        if (members.find(membernum) != members.end())
+        {
+            return membernum;
+        }
+        else continue;
+    }
+}
+
+void Applications::FindMember(int membernumber)
+{
+    map<int, string>::iterator it;
+    map <int, int>::iterator itr;
+    it = members.find(membernumber);
+    itr = result.find(membernumber);
+    if (it != members.end() && itr != result.end()) {
+        cout << "Member number\t Member\t Result" << endl;
+        cout << (*it).first << "\t " << (*it).second << "\t " << (*itr).second << endl;
+    }else cout << "There is no such member";
+}
+
+bool userlogin()
+{
 
 }
 
-int menu()
+void adminmenu()
 {
     Applications application;
-    
-    cout << application.members;
+
+    while (true)
+    {
+        system("cls");
+        cout << "What do you need?\n" << "1)Add member;\n" << "2)Add result;\n" << "3)Find member;\n" << "4)Display a table of members;\n" << "5)Printing of the certificate;\n";
+        int choice;
+        cin >> choice;
+        if (choice == 1)
+        {
+            pair <int, string> member;
+            member.first = application.GenMemberNumber();
+            cout << "Enter the full name of member:" << endl;
+            getline(cin, member.second);
+            member.first = application.GenMemberNumber();
+            AddMember(application, member);
+        }
+        else if (choice == 2)
+        {
+            pair <int, int> result;
+            cout << "Enter the member number: ";
+                cin >> result.first;
+
+            cout << "Enter the member result: ";
+                cin >> result.second;
+            
+                AddResult(application, result);
+        }
+        else if (choice == 3)
+        {
+            int membernum;
+            cout << "Enter the member number: ";
+                cin >> membernum;
+
+
+            
+        }
+        else if (choice == 4)
+        {
+
+        }
+        else if (choice == 5)
+        {
+
+        }
+        
+    }
+
 }
 
 int main()
 {
-    menu();
+    userlogin();
 }
